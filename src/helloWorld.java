38df946c4968af8c79f172333e0e6b2d63b97013
danawacomputer <loveyou_06@naver.com>
2017-04-04 11:12:28 +0900

    //1차업로드

---
+++ src/helloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Created by danawacomputer on 2017-04-03.
+ * PM 3:45
+ * 목표: HelloWorld 출력하기
+ * 응용: 총 수업시간 출력하기
+ */
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld 한글출력");
+
+        int nHowManyDays = 90;
+        int nOneDayHours = 8;
+        int nTotalHours = 0;
+        nTotalHours = nHowManyDays * nOneDayHours;
+        System.out.println("총 수업시간은 "+nTotalHours+"시간 입니다.");
+        System.out.println("추가");
+        System.out.println("추가11:11");
+        System.out.println("추가11:12");
+    }
+}
