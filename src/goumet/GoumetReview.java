feace30872967e7db2ace2bd64cebc8aea78d0fb
danawacomputer <loveyou_06@naver.com>
2017-04-10 11:42:37 +0900

    //20170410

--- src/goumet/GoumetReview.java
+++ src/goumet/GoumetReview.java
@@ -1,5 +1,7 @@
 package goumet;
 
+import com.sun.org.apache.bcel.internal.generic.FieldGenOrMethodGen;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +33,7 @@ public class GoumetReview {
         ironPlate.setScore(46);
 
         //데이터메소드에서 형식을 지정하면 객체를 생성하면서 데이터를 바로 입력할 수 있다.
-        Goumet yongho = new Goumet("용호낙지","낙지,낙곱새",46);
+        Goumet yongho = new Goumet("용호낙지", "낙지,낙곱새", 46);
 
         List<Goumet> list = new ArrayList<>();
         list.add(spoon);
@@ -44,5 +46,24 @@ public class GoumetReview {
         /* 맛집 점수의 평균 구하기 */
         GoumentService scoreTotal = new GoumentService();
         scoreTotal.calcAverageScore(list);
+
+        Goumet g1 = new Goumet("a", "b", 25);
+        Goumet g2 = new Goumet("a", "b", 30);
+
+        g1.getScore();
+        g2.getScore();
+        //같은 방식으로 호출하였으나 결과값이 다르다.
+        // 함수를 호출했을 때 출력값이 다르므로. 멤버변수를 사용하므로
+        // 퓨어펑션이 아니므로 메소드라고 한다.
+        // 입력할 수 있는 데이터가 상황에 따라서 바뀔 수 있는 경우에 메소드라고 정의한다.
+
+        int randomSumResult = 0;
+        for(int i=0; i<100; i++) {
+            int r = (int) (Math.random() * 6 + 1);
+            randomSumResult = r + randomSumResult;
+            System.out.printf("%d: %d\n",i+1,r);
+        }
+        System.out.println("합계: "+randomSumResult);
+        System.out.println("평균: "+randomSumResult/100);
     }
 }
